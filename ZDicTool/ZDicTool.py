ff6ed0d2c63093290c67a3fdbf975b708590ba81
osfans <osfans@28f5bac7-8d37-0410-befc-a79d5cd9e343>
2008-11-18 16:55:56 +0000

    OS: Add bsddb module (-b parameter) in ZDic Tool script for memory error, and combine same words with several meanings.
    git-svn-id: http://svn.code.sf.net/p/zdic/code/trunk@38 28f5bac7-8d37-0410-befc-a79d5cd9e343

--- ZDicTool/ZDicTool.py
+++ ZDicTool/ZDicTool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: cp936 -*-
 
-import os, sys, getopt, bz2, time, datetime, re
+import os, sys, getopt, bz2, time, datetime, re, htmlentitydefs, bsddb
 from struct import *
 
 PDBHeaderStructString='>32shhLLLlll4s4sllH'
@@ -8,7 +9,26 @@ PDBHeaderStructLength=calcsize(PDBHeaderStructString)
 padding='\0\0'
 
 enc = sys.getfilesystemencoding()
-enc = 'gbk' if enc == 'UTF-8' else enc
+enc = 'gbk' if enc == 'UTF-8' else enc #默认编码
+
+bsddb_opt = False
+
+replace_dic = {} #要转换的HTML命名
+for name, codepoint in htmlentitydefs.name2codepoint.items():
+    try:
+        char = unichr(codepoint).encode(enc)
+        replace_dic["&%s;" % name.lower()]=char
+    except:
+        pass
+
+replace_ste =[('<hr>','//STEHORIZONTALLINE//\n'),
+              ('<center>','//STECENTERALIGN//'),('</center>','\n'),
+              ('<u>',"//STEHYPERLINK="),('</u>',"\1//"),
+              ('<i>',"//STEBOLDFONT//"),('</i>',"//STESTDFONT//"),
+              ('<b>',"//STEBOLDFONT//"),('</b>',"//STESTDFONT//"),
+              ('<big>',"//STEBOLDFONT//"),('</big>',"//STESTDFONT//")]#可替换成STE的标记
+    
+removed_title=['Help', 'Template', 'Image', 'Portal', 'Wikipedia', 'MediaWiki', 'WP'] #要删除的词条
   
 class ZDic:
     def __init__(self,compressFlag=2,recordSize=0x4000):
@@ -22,19 +42,16 @@ class ZDic:
         self.compressFlag=compressFlag
 
     def trim(self,s):
-        s = s.decode('utf-8').encode(enc,'replace').replace("&amp;", "&").replace("&lt;", "<").replace("&gt;", ">")\
-                .replace("&apos;", "'").replace("&quot;", '"').replace("&nbsp;", " ")
+        s = s.decode('utf-8').encode(enc,'replace').replace('&amp;', '&')
+        for name, char in replace_dic.iteritems():
+            s = s.replace(name, char)
         s = re.compile('(</?(p|font|br|tr|td|table|div|span|ref|small).*?>)|(\[\[[a-z]{2,3}(-[a-z]*?)?:[^\]]*?\]\])|(<!--.*?-->)',\
                         re.I|re.DOTALL).sub('',s)
         return s
 
     def ste(self,s):       
-        s = s.replace('<hr>','//STEHORIZONTALLINE//\n')\
-                .replace('<center>','//STECENTERALIGN//').replace('</center>','\n')\
-                .replace('<u>',"//STEHYPERLINK=").replace('</u>',"\1//")\
-                .replace('<i>',"//STEBOLDFONT//").replace('</i>',"//STESTDFONT//")\
-                .replace('<b>',"//STEBOLDFONT//").replace('</b>',"//STESTDFONT//")\
-                .replace('<big>',"//STEBOLDFONT//").replace('</big>',"//STESTDFONT//")       
+        for i, j in replace_ste:
+            s = s.replace(i, j)     
         s = re.sub('\[\[([^\[/]*?)\]\]',"//STEHYPERLINK=\g<1>\1//", s)        
         s = re.sub('\n{3,}','\n\n',s).replace('\n',r'\n')
         return s
@@ -76,28 +93,31 @@ class ZDic:
         f.close()
 
     def fromPath(self, path):
-        files = []        
+        files = [] #保存所有要转换的文件       
         if os.path.isdir(path):
             files = glob.glob(os.path.join(path, '*.*'))
         else:
             files = [path] 
-        self.lines = {}
+        self.lines = bsddb.btopen('tmp.db','c') if bsddb_opt else {}
         for path in files:
-            ext=os.path.splitext(path)[1].lower()
-            if ext in ['.xml', '.bz2']:#wiki xml
-                f=bz2.BZ2File(path) if ext=='.bz2' else open(path)
-                block=1024*32
-                s=f.read(block)
-                tmp=''
+            ext=os.path.splitext(path)[1].lower()#获取后缀名
+            if ext in ['.xml', '.bz2']: #wiki xml
+                f = bz2.BZ2File(path) if ext == '.bz2' else open(path) #打开压缩或者不压缩格式均可
+                block = 1024*32 #每次读入32K
+                s = f.read(block)
+                tmp = ''
                 while 1:  
                     try:
-                        a=s.index('<title>')
-                        b=s.index('</title>',a)
-                        c=s.index('<text xml:space="preserve">',b)
-                        d=s.index('</text>',c)
-                        word,mean=self.trim(s[a+7:b]),self.trim(s[c+27:d])
-                        if word in self.lines and mean[:9].lower()=='#redirect':
+                        a = s.index('<title>')
+                        b = s.index('</title>',a)
+                        c = s.index('<text xml:space="preserve">',b)
+                        d = s.index('</text>',c)
+                        word, mean = self.trim(s[a+7:b]), self.trim(s[c+27:d])
+                        if ((word in self.lines) and (mean[:9].lower()=='#redirect')) \
+                           or ((':' in word) and word[:word.index(':')] in removed_title):#跳过简繁转换后的重复词条或跳过某些类别词条
                             pass
+                        elif word in self.lines:
+                            self.lines[word]+='\\n\\n\\n' + self.ste(mean) #重复词条，合并成一条
                         else:
                             self.lines[word]=self.ste(mean)
                         s=s[d:]
@@ -116,7 +136,14 @@ class ZDic:
                     i = i.rstrip().replace(' /// ', '\t', 1)
                     if '\t' in i:
                         word, mean = i.split('\t', 1)
-                        self.lines[word] = self.ste(mean)               
+                        if word in self.lines:
+                            self.lines[word]+='\\n\\n\\n' + self.ste(mean) #重复词条，合并成一条
+                        else:
+                            self.lines[word] = self.ste(mean)
+        if bsddb_opt:
+            self.lines.sync()
+            self.lines.close()
+            
 
     def resizeBlock(self):
         tmp=''
@@ -164,6 +191,62 @@ class ZDic:
         if tmp:
             self.compress(tmp,first.split('\t',1)[0],line.rsplit('\n',2)[-2].split('\t',1)[0],f)
         f.close()
+
+    def resizeBlockB(self):
+        """bsddb version by emfox"""
+        tmp=''
+        first=''
+        f=open('tmp.pdb','wb')
+        dbf = bsddb.btopen('tmp.db','w')
+        if '' not in dbf:
+            dbf['']=self.ste('<b>Welcome to //STEPURPLEFONT//%s//STECURRENTFONT//!</b>\n<b>Words count:</b> //STEBLUEFONT//%d//STECURRENTFONT//\n<b>Made time:</b> //STEREDFONT//%s'%(self.pdbName,len(dbf),time.strftime("%Y.%m.%d")))
+        word,mean = dbf.first()
+        while 1:
+            line = "%s\t%s\n"%(word,mean)
+            if first=='':
+                first=tmp
+            if len(tmp+line)<self.recordSize:
+                tmp+=line
+            else:
+                if tmp:
+                    self.compress(tmp,first.split('\t',1)[0],tmp.rsplit('\n',2)[-2].split('\t',1)[0],f)
+                    tmp=''
+                if len(line)>=self.recordSize:
+                    index,line=line.split('\t',1)
+                    i=0
+                    while line:
+                        tmpindex=i and "%s %d"%(index,i) or index
+                        blen=self.recordSize-len(tmpindex)-3
+                        if blen>len(line):
+                            tmpline=line
+                            line=''
+                        else:
+                            if '//STE' in line[blen-40:blen+4]: #split form //STE
+                                steindex=line[blen-40:blen+4].index('//STE')
+                                blen=blen-40+steindex
+                            elif '\\n' == line[blen-1:blen+1]: # split from \n
+                                blen=blen-1
+                            tmpline=line[:blen]
+                            line=line[blen:]
+                            try:
+                                tmpline.decode('gbk')
+                            except:
+                                line=tmpline[-1]+line
+                                tmpline=tmpline[:-1]                           
+                        self.compress("%s\t%s\n"%(tmpindex,tmpline),tmpindex,tmpindex,f)
+                        i+=1
+                else:
+                    tmp=line
+                first=''
+            try:
+                word,mean = dbf.next()
+            except:
+                break
+        if tmp:
+            self.compress(tmp,first.split('\t',1)[0],line.rsplit('\n',2)[-2].split('\t',1)[0],f)
+        dbf.close()
+        f.close()
+        os.remove('tmp.db')
             
     def compress(self,block,first,last,f):
         block=block.encode('zlib')
@@ -205,7 +288,7 @@ class ZDic:
         #header and index
         f.write(towrite+self.index)
         del towrite,self.index
-        block=1024*1024*10
+        block=1024*1024*10 #
         tmp=open('tmp.pdb','rb')
         while 1:
             cont=tmp.read(block)
@@ -213,9 +296,9 @@ class ZDic:
                 f.write(cont)
             else:
                 break
-        tmp.close()
+        tmp.close()        
+        os.remove('tmp.pdb')
         f.close()
-        os.remove('tmp.pdb')     
 
     def p2t(self, path, patho):
         "pdb=>byteList"
@@ -246,6 +329,7 @@ class ZDic:
             f.close()
 
 def log(msg):
+    """打印日志"""
     print '[%s]%s'%(time.strftime('%X'), msg)
     
 if __name__ == '__main__':    
@@ -253,20 +337,24 @@ if __name__ == '__main__':
     if len(argv) == 2:
         pathi, patho = argv
         try:
-            s = time.time()
+            s = time.time() #记录开始时间
             log('Loading...')
-            app = ZDic()
+            app = ZDic()    #初使化ZDic数据结构
             app.pdbName = os.path.splitext(os.path.basename(patho))[0]
-            if ('-t', '') in opts:
+            if ('-t', '') in opts: #-t参数：转换为文本文件
                 log('Processing...')
                 app.p2t(pathi, patho)
             else:
+                bsddb_opt = ('-b', '') in opts #-b参数：使用bsddb，较小内存，较大文件
                 app.fromPath(pathi)
                 log('Processing...')
-                app.resizeBlock()
+                if bsddb_opt:
+                    app.resizeBlockB()
+                else:
+                    app.resizeBlock()
                 log('Saving...')
-                app.toPDB(patho)
-            cost = time.time() - s
+                app.toPDB(patho)                
+            cost = time.time() - s #计算花费时间
             log('Success! %dh%dm%ds passed.' % (cost/3600, cost%3600/60, cost%60))
         except:
             log('Error!')
